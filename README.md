3ed4bb266af0559b515acd3878ee58ba9fcb1cd1
Deepak Bansode <deepak.bansode@frontdoorhome.com>
2021-05-13 20:44:31 +0530

    setup the project and connect to database

---
+++ README.md
@@ -0,0 +1,10 @@
+POST /todos             to add new todo
+PATCH /todos/{id}       to update todo by id
+DELETE /todos/{id}      to delete todo by id
+GET /todos/{id}         to get todo by id
+GET /todos              to list todo items by search criteria
+
+
+
+Run the app
+docker-compose up --build
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"go-todos/config"
+	"go-todos/database"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	conf := config.GetConfig()
+	db := database.ConnectDB(conf.Mongo)
+	fmt.Println(db)
+
+	r := mux.NewRouter()
+
+	http.ListenAndServe(":8080", r)
+}
